d93bee99a7bb7bd2dddcb9d678cbf0a0a4bb3930
rauan <ommirkulr@gmail.com>
2024-07-04 15:59:43 +0500

    small changes

--- parser.py
+++ parser.py
@@ -1,5 +1,25 @@
+import json
 import requests
 
-rep = requests.get("https://youtube.com")
+# Send a GET request to the specified URL
+rep = requests.get("https://dikhanbabanan.kz/")
 
-print(rep.text)
+# Ensure the response is in JSON format
+try:
+    data = rep.text
+except ValueError:
+    print("Error: Response is not in JSON format")
+    data = None
+
+if data:
+    # Open a file in write mode
+    with open("data.json", "w") as out_file:
+        # Dump the JSON data to the file with indentation for readability
+        json.dump(data, out_file, indent=4)
+
+#rep = requests.get("https://www.youtube.com/results?", params={'search_query':'rauan'})
+
+def parse_for_all_objects(html):
+    """Parses input html to find all objects of the videos"""
+    result = []
+
